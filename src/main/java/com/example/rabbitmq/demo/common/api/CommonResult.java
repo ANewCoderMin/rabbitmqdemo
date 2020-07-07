c774e11c58a2a55e2e8d03ba4ce610bd4798e7e5
uisftech <18295766117@163.com>
2020-07-07 17:45:48 +0800

    1.0

--- src/main/java/com/example/rabbitmq/demo/common/api/CommonResult.java
+++ src/main/java/com/example/rabbitmq/demo/common/api/CommonResult.java
@@ -1,7 +1,17 @@
 package com.example.rabbitmq.demo.common.api;
 
-public class CommonResult {
+public class CommonResult<T> {
 
+    private long code;
+    private String status;
+    private T data;
+    public static <T> CommonResult<T> success(String status){
 
+        return new CommonResult<>(200L,status);
+    }
 
+    public CommonResult(long code, String status) {
+        this.code = code;
+        this.status = status;
+    }
 }
--- src/main/java/com/example/rabbitmq/demo/service/impl/OmsPortalOrderServiceImpl.java
+++ src/main/java/com/example/rabbitmq/demo/service/impl/OmsPortalOrderServiceImpl.java
@@ -2,23 +2,34 @@ package com.example.rabbitmq.demo.service.impl;
 
 import com.example.rabbitmq.demo.common.api.CommonResult;
 import com.example.rabbitmq.demo.common.api.OrderParam;
+import com.example.rabbitmq.demo.component.CancelOrderSender;
 import com.example.rabbitmq.demo.service.OmsPortalOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OmsPortalOrderServiceImpl implements OmsPortalOrderService {
 
     private static Logger LOGGER = LoggerFactory.getLogger(OmsPortalOrderService.class);
-
+    @Autowired
+    private CancelOrderSender cancelOrderSender;
     @Override
     public CommonResult generateOrder(OrderParam OrderParam) {
-        return null;
+
+        LOGGER.info("process generateOrder");
+        sendDelayMessage(11L);
+        return CommonResult.success("下单成功");
     }
 
     @Override
     public void cancelOrder(Long orderId) {
+        LOGGER.info("order cancel",orderId);
+    }
 
+    private void sendDelayMessage(long orderId) {
+        long delayTimes = 6 * 1000;
+        cancelOrderSender.sendMessage(orderId,delayTimes);
     }
 }
